7c22267affe13df35e9577af5c4341872b99c0c2
raty918@gmail.com <raty918@gmail.com>
2017-06-27 15:37:13 +0200

    v1

---
+++ arachnijenkinsfile/alljobs/scan.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+SCAN_OPTIONS=" $3 --spawns="$4" --grid-mode="$5" --checks="$9" --report-save-path=/home/arachniwebreport/reportafr/$8.afr"
+
+#SCOPE_DIRECTORY_DEPTH_LIMIT option
+if [ "$6" != "inf" ]; 
+        then
+                SCAN_OPTIONS="$SCAN_OPTIONS --scope-directory-depth-limit=$6"
+fi
+
+#SCOPE_PAGE_LIMIT option
+if [ "$7" != "inf" ]; 
+        then
+                SCAN_OPTIONS="$SCAN_OPTIONS --scope-page-limit=$7"
+fi
+        
+#Send scan command to the RPC Server
+if [ "$1" != "default" ]; #DISPATCHER_GRID_HOST not set
+        then
+		( exec "./wait-for-it.sh -t 0 $1" )	
+                docker -H $2 exec -t v4_web_1 bin/arachni_rpc --dispatcher-url $1 $SCAN_OPTIONS   
+        else  
+                add=$(docker -H $2 inspect v4_dispatcher_grid_1 | grep IPAddress | cut -d '"' -f 4):7331
+                ( exec "./wait-for-it.sh -t 0 $add" )	
+	        docker -H $2 exec -t v4_web_1 bin/arachni_rpc --dispatcher-url $add $SCAN_OPTIONS
+fi
+
